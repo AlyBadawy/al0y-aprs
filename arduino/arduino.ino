77a7f9b4fbbfc29e24f78de27932090b312af885
= <=>
2022-04-07 21:37:36 -0400

    Updated library

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,12 @@
 //include the libraries
 #include <dht.h>
+//#include <LiquidCrystal_I2C.h>
 //#include <Wire.h> 
 
 dht DHT;//create a variable type of dht
+//LiquidCrystal_I2C lcd(0x27,16,2); 
 
-int   DHT11_PIN = 17;//Humiture sensor attach to pin5
+int   DHT11_PIN = A3;
 int   BA_PIN    = A4;
 int   PS_PIN    = A5;
 float BA_Volt   = 0.0;
@@ -17,11 +19,21 @@ void setup()
   pinMode(PS_PIN,INPUT);//set the state of A0 as INPUT
   pinMode(BA_PIN,INPUT);//set the state of A1 as INPUT
   
-  Serial.begin(9600);//initialize the serial
-
-  PS_Volt = analogRead(PS_PIN) * (5.0 / 1023.0) * 4;
+  Serial.begin(9600); //initialize the serial
+//  lcd.init(); //initialize the lcd
+//  lcd.backlight(); //open the backlight 
+  
+}
+  
+void loop()
+{
+  analogRead(PS_PIN); 
   delay(20);
-  BA_Volt = analogRead(BA_PIN) * (5.0 / 1023.0) * 4;
+  PS_Volt = analogRead(PS_PIN) * (4.7 / 1024.0) * 4.0;
+  delay(30);
+  analogRead(BA_PIN); 
+  delay(20);
+  BA_Volt = analogRead(BA_PIN) * (4.7 / 1024.0) * 4.0;
   
   int chk = DHT.read11(DHT11_PIN);//read the value returned from sensor
   if (chk == DHTLIB_OK) {
@@ -35,14 +47,29 @@ void setup()
     output += String(BA_Volt, 1);
     output += "@";
     Serial.println(output);
-    pinMode(LED_BUILTIN, OUTPUT);
-  }
-}
-  
-void loop()
-{
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
+//    lcd.clear();
+//    
+//    lcd.setCursor(0, 0);
+//    lcd.print("T:");
+//    lcd.print((DHT.temperature *1.8) + 32, 1); //print the temperature on lcd
+//    lcd.print(char(223));//print the unit" ℃ "
+//    lcd.print("F");
+//    
+//    lcd.setCursor(9, 0);
+//    lcd.print("H:");
+//    lcd.print(DHT.humidity, 1); //print the humidity on lcd
+//    lcd.print("%"); 
+//  
+//    lcd.setCursor(0, 1);
+//    lcd.print("PS:");
+//    lcd.print(PS_Volt,1);
+//    lcd.print("V");
+//  
+//    lcd.setCursor(9, 1);
+//    lcd.print("B:");
+//    lcd.print(BA_Volt,1);
+//    lcd.print("V");
+    delay(1000);
+  } 
 }
